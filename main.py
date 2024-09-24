9a9d8cbedc7f772b9c5ce7aabfcacec9c5ff3761
astaluc <asta.luciunaite@gmail.com>
2024-09-24 19:41:16 +0300

    Uzduotys up to 7

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ if 5 > 4 and 3 < 8 and 7 < 14:
 if 5 > 4 or 3 < 8 or 7 > 14:
     print("or")
 
-print("Salyginiia sakiniai")
-print("Uzduotys 2/3")
+print("Salyginiia sakiniai if")
+print("Uzduotys 2/3.2-3")
 '''
 print("Iveskite amziu")
 amzius = int(input())
@@ -34,10 +34,50 @@ print(sum([paz1, paz2, paz3]) / len([paz1, paz2, paz3]))
 vidurkis = sum([paz1, paz2, paz3]) / len([paz1, paz2, paz3])
 if vidurkis > 0 or vidurkis == 5:
     print("vidurkis teigiamas")
+print( input() )
 '''
-print("Uzduotys 3/3")
+print("Uzduotys 3/3.4")
 sk = 6
 if sk % 5 == 0:
     print("\n", sk * 1, "\n", sk * 2, "\n", sk * 3, "\n", sk * 4, "\n", sk * 5)
 if sk % 2 == 0:
     print("\n", sk, "\n", sk ** 2, "\n", sk / 2, "\n", math.sqrt(sk))
+
+print()
+print("Salyginiia sakiniai if_elif")
+print("Uzduotys 1/2.5")
+sk1 = 5
+sk2 = 10
+sk3 = 15
+if sk1 > sk2:
+    print("sk1 > sk2")
+elif sk2 > sk3:
+    print("sk2 > sk3")
+elif sk3 > sk1:
+    print("sk3 > sk1")
+
+print("Uzduotys 2/2.6")
+if sk1 == sk2:
+    print("sk1 = sk2")
+elif sk2 == sk3:
+    print("sk2 = sk3")
+elif sk1 == 0:
+    print("sk1 = 0")
+elif sk2 < 0:
+    print("sk2 < 0")
+elif sk3 > 0:
+    print("sk3 > 0")
+
+print("Uzduotys 2/3.7")
+paz = 8
+if paz == 10:
+    print("puiku")
+elif paz >= 9:
+    print("labai gerai")
+elif paz >= 7:
+    print("gerai")
+elif paz >= 5:
+    print("patenkinamai")
+elif paz < 5:
+    print("egzaminas neislaikytas")
+
